f85206393324e4c161bdd9d9f116f020d1bcde42
Thailan Godoy <thailanrg@gmail.com>
2026-05-13 18:42:24 -0300

    additing ia files

---
+++ main.py
@@ -0,0 +1,23 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+numero_dias = 7
+numero_criancas = 2
+atividade = "praia"
+
+prompt = f"Crie um roteiro de viagens, para um periodo de {numero_dias}, para uma familia com {numero_dias} que busca atividades relacionado a {atividade}"
+
+modelo = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0.5,
+    api_key=api_key
+)
+
+resposta = modelo.invoke(prompt)
+
+print(resposta.content)
